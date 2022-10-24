8419f3ff0497c6b86cfdd46261315352c779abcc
Lokesh Pereiro <lpdraft001@gmail.com>
2022-10-24 10:56:49 +0200

    pill resolved

---
+++ src/15-exercise/15-exercise.js
@@ -0,0 +1,19 @@
+/**
+ * Exercise 15:
+ *
+ * 1. Create a `divide` function that receives two number
+ *    parameters and returns the division of both of them
+ *
+ * 2. Export the function as a default export
+ *
+ * @example
+ *
+ * function test() {
+ *    return "something";
+ * };
+ *
+ * export default test;
+ */
+export function divide(a, b) {
+    return a / b;
+}
